1d03a733036a5061bd1d515f54ac12052f61b54b
Peter Green <peter.green@plextrac.com>
2022-07-26 12:26:41 -0600

    added general json file load function

--- input_utils.py
+++ input_utils.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 
@@ -5,10 +6,11 @@ from request_utils import *
 
 
 prompt_prefix = "\n[Prompt] "
+prompt_suffix = ": "
 
 # prompts user for data not needing validation
 def prompt_user(msg):
-    return input(prompt_prefix + msg)
+    return input(prompt_prefix + msg + ": ")
 
 
 # prompts users for an input. checks the entered input against valid options. returns a valid choice
@@ -20,7 +22,7 @@ def prompt_user_options(msg, retry_msg="", options=[]):
     str_options = str_options[0:-1]
     
     #get input
-    entered = input(prompt_prefix + msg + " (" + str_options + "): ")
+    entered = input(prompt_prefix + msg + " (" + str_options + ")" + prompt_suffix)
     
     #validate input
     if entered in options:
@@ -31,9 +33,26 @@ def prompt_user_options(msg, retry_msg="", options=[]):
         return prompt_user_options(msg, retry_msg, options)
 
 
+# prompts users for an input. checks the entered input is in a valid range. returns a valid choice
+def prompt_user_list(msg, retry_msg="", range=0):
+    #setup
+    str_options = "0-" + str(range)
+    
+    #get input
+    entered = input(prompt_prefix + msg + " (" + str_options + ")" + prompt_suffix)
+    
+    #validate input
+    if int(entered) >= 1 and int(entered) < range:
+        return int(entered)
+
+    #ask again
+    if prompt_retry(retry_msg):
+        return prompt_user_list(msg, retry_msg, range)
+
+
 # prompts user if they want to ignore the last, potentially problematic, input option
 def prompt_continue_anyways(msg):
-    entered = input(prompt_prefix + msg + " Continue Anyways? (y/n): ")
+    entered = input(prompt_prefix + msg + " Continue Anyways? (y/n)" + prompt_suffix)
     if entered == 'y':
         return True
     elif entered == 'n':
@@ -44,7 +63,7 @@ def prompt_continue_anyways(msg):
         
 # prompts the users if they want to retry the last input option
 def prompt_retry(msg):
-    entered = input(prompt_prefix + msg + " Try Again? (y/n): ")
+    entered = input(prompt_prefix + msg + " Try Again? (y/n)" + prompt_suffix)
     if entered == 'y':
         return True
     elif entered == 'n':
@@ -61,7 +80,7 @@ def prompt_retry(msg):
 
 # prompts user for their plextrac url, checks that the API is up and running, then returns the url
 def handle_instance_url():
-    base_url = prompt_user("Please enter the full URL of your PlexTrac instance (with protocol): ")
+    base_url = prompt_user("Please enter the full URL of your PlexTrac instance (with protocol)")
 
     #validate
     try:
@@ -94,7 +113,7 @@ def handle_instance_url():
 # handles extra layer of Cloudflare authorization
 # plextrac test instances are hosted behind a Cloudflare wall that requires another layer of authorization
 def handle_cf_instance_url(base_url):
-    cf_token = prompt_user("Please enter your active 'CF_Authorization' token: ")
+    cf_token = prompt_user("Please enter your active 'CF_Authorization' token")
     response = requests.get(f'{base_url}/api/v1/', headers={"cf-access-token": cf_token}) # could be refractored to use request utils
             
     try:
@@ -109,3 +128,21 @@ def handle_cf_instance_url(base_url):
     except Exception as e:
         if prompt_retry("Could not validate instance URL."):
             return handle_instance_url()
+
+
+# gets the file path of a json to be imported, checks if the file exists, and trys to load and return the data
+def handle_load_json_data(msg):
+    json_file_path = prompt_user(prompt_prefix + msg + "(relative file path, including file extention)" + prompt_suffix)
+
+    if not os.path.exists(json_file_path):
+        if prompt_retry(f'Specified JSON file at \'{json_file_path}\' does not exist.'):
+            return handle_load_json_data(msg)
+    
+    try:
+        with open(json_file_path, 'r', encoding="utf8") as file:
+            json_data = json.load(file)
+    except Exception as e:
+        if prompt_retry(f'Error loading file: {e}'):
+            return handle_load_json_data(msg)
+    
+    return json_data
