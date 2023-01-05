7681b7bd4ee47251db70234aaaec8fec38809906
Peter Green <peter.green@plextrac.com>
2023-01-05 10:21:25 -0700

    Added proper logging
    Implemented a python logger and wrapper to display logs by color coding
    
    Logs are displayed on terminal with option to save logs to file
    Saving logs to file is turned off by default. This can be enabled in the
    settings.py file by changing the params when creating the LogFormatHandler

--- README.md
+++ README.md
@@ -17,17 +17,15 @@ pipenv install //this will create a virtual env and install all the dependancies
 # Usage
 After the Python environment is setup, you can run the script with the following command. You should be in the folder where you cloned the repo when running the following.
 ```bash
-pipenv run python example_authenticate.py
+pipenv run python main.py
 ```
+
+# Config
+You can add the following values to the config.yaml file. If a value is not set, you will be prompted to enter it when the script runs.
 
 ## Required Information
 - PlexTrac Top Level Domain e.g. https://yourapp.plextrac.com
 - Username
 - Password
 - MFA Token (if enabled)
-
-## Script Execution Flow
-- Prompts user for Plextrac instance URL
-  - Validate URL points to a running instance of Plextrac
-- Prompts user for username, password, and mfa (if applicable)
-- Calls authenticate endpoints and stores Authoirzation headers for future use
+- Client Name
--- request_utils.py
+++ request_utils.py
@@ -1,25 +1,28 @@
 import requests
 import json
 
+import settings
+log = settings.log
+
 # if the Plextrac instance is running on https without valid certs, requests will respond with cert error
 # change this to false to override verification of certs
 verify_ssl = True
 
 # call if the api call fails, generally 500 codes
 def err_requests_general(request, type, path, e):
-    print(f'Err: Could not complete {type} \'{request}\' on \'{path}\' endpoint. Exception: {e}')
+    log.error(f'Could not complete {type} \'{request}\' on \'{path}\' endpoint. Exception: {e}')
 
 # call when the api call returns a non success, generally 400 codes
 def err_non_200_response(request, status, reason):
-    print(f'Err: \'{request}\' request failed with status: {status} - {reason}')
+    log.error(f'\'{request}\' request failed with status: {status} - {reason}')
 
 # call when the api call doesn't return a valid json response, if a json response was expected
 def err_invalid_json_response(request, e):
-    print(f'Err: Malformed \'{request}\' API response. Exception: {e}')
+    log.error(f'Malformed \'{request}\' API response. Exception: {e}')
 
 # call when the api call is missing a specific field, when that field is expected
 def err_missing_required_response_field(request, field):
-    print(f'Err: Received empty \'{field}\' value from server')
+    log.error(f'Received empty \'{field}\' value from server')
 
 
 # general GET request with error messages inserted
@@ -37,7 +40,7 @@ def request_get(base_url, request_root, request_path, request_name, headers):
 
             if response.status_code != 200:
                 err_non_200_response(request_name, response.status_code, response.reason)
-                print(f'Plextrac message: {response_json.get("message")}')
+                log.warn(f'Plextrac message: {response_json.get("message")}')
         except Exception as e:
             err_invalid_json_response(request_name, e)
             return response
@@ -64,7 +67,34 @@ def request_post(base_url, request_root, request_path, request_name, headers, pa
 
             if response.status_code != 200:
                 err_non_200_response(request_name, response.status_code, response.reason)
-                print(f'Plextrac message: {response_json.get("message")}')
+                log.warn(f'Plextrac message: {response_json.get("message")}')
+        except Exception as e:
+            err_invalid_json_response(request_name, e)
+            return response
+
+        return response_json
+        
+    except Exception as e:
+        err_requests_general(request_name, "POST", request_path, e)
+        exit()
+
+
+# POST request for multipart form-data with error messages inserted
+def request_post_multipart(base_url, request_root, request_path, request_name, headers, multipart_form_data):
+    try:
+        response = requests.post(
+            f'{base_url}{request_root}{request_path}',
+            headers = headers,
+            files = multipart_form_data,
+            verify = verify_ssl
+        )
+
+        try:
+            response_json = json.loads(response.text)
+
+            if response.status_code != 200:
+                err_non_200_response(request_name, response.status_code, response.reason)
+                log.warn(f'Plextrac message: {response_json.get("message")}')
         except Exception as e:
             err_invalid_json_response(request_name, e)
             return response
@@ -91,7 +121,7 @@ def request_put(base_url, request_root, request_path, request_name, headers, pay
 
             if response.status_code != 200:
                 err_non_200_response(request_name, response.status_code, response.reason)
-                print(f'Plextrac message: {response_json.get("message")}')
+                log.warn(f'Plextrac message: {response_json.get("message")}')
         except Exception as e:
             err_invalid_json_response(request_name, e)
             return response
@@ -118,7 +148,7 @@ def request_delete(base_url, request_root, request_path, request_name, headers):
 
             if response.status_code != 200:
                 err_non_200_response(request_name, response.status_code, response.reason)
-                print(f'Plextrac message: {response_json.get("message")}')
+                log.warn(f'Plextrac message: {response_json.get("message")}')
         except Exception as e:
             err_invalid_json_response(request_name, e)
             return response
@@ -148,3 +178,29 @@ def request_mfa_authenticate(base_url, headers, payload):
     root = "/api/v1"
     path = "/authenticate/mfa"
     return request_post(base_url, root, path, name, headers, payload)
+
+#----------Client Endpoints----------
+def request_list_clients(base_url, headers):
+    name = "List Clients"
+    root = "/api/v1"
+    path = "/client/list"
+    return request_get(base_url, root, path, name, headers)
+
+def request_get_client(base_url, headers, client_id):
+    name = "Get Client"
+    root = "/api/v1"
+    path = f'/client/{client_id}'
+    return request_get(base_url, root, path, name, headers)
+
+#----------Report Endpoints----------
+def request_list_client_reports(base_url, headers, client_id):
+    name = "List Client Reports"
+    root = "/api/v1"
+    path = f'/client/{client_id}/reports'
+    return request_get(base_url, root, path, name, headers)
+
+def request_export_report_to_ptrac(base_url, headers, client_id, report_id):
+    name = "Export Report to Ptrac"
+    root = "/api/v1"
+    path = f'/client/{client_id}/report/{report_id}/export/ptrac'
+    return request_get(base_url, root, path, name, headers)
